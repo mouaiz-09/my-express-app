3624e77bce026874244a0158cc27ec0dde5186ee
mouaiz <rlqyyn@gmail.com>
2025-03-22 15:38:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 // get  data 
 app.get("/user", (req, res) => {
     if (user.length === 0) {
-        res.status(404).send("  <img srcset="" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIABjhEHIhqK--1h7fFhi-gimvD8S0spRiPs4qDrEgv2GUI8lzvowyEEs&s=10" alt="">
+        res.send("  <img srcset="" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIABjhEHIhqK--1h7fFhi-gimvD8S0spRiPs4qDrEgv2GUI8lzvowyEEs&s=10" alt="">
   ")
 
     }
